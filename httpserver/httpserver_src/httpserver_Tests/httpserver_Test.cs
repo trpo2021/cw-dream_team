7d040dd0febe3bc87dc6677572cf7bbc10cb3bbb
Ebisu02 <71516051+Ebisu02@users.noreply.github.com>
2021-05-22 18:52:02 +0700

    Rework file system in server, add dir for tests

---
+++ httpserver/httpserver_src/httpserver_Tests/httpserver_Test.cs
@@ -0,0 +1,16 @@
+using Xunit;
+using System.Net.Http;
+using httpserver;
+
+namespace httpserver_Tests
+{
+    public class httpserver_Test
+    {
+        [Fact]
+        public void TestOfDataInDocument()
+        {
+            
+        }
+      
+    }
+}

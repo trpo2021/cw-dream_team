7c7b30e1930003986095ba5fa85c7b45394433e4
Karen <karen0089@mail.ru>
2021-05-28 22:19:05 +0700

    Add class what realise a function what setup our directory as working or check it if already setup

---
+++ WorkWithFiles/WorkingDirectory/WorkingDirectory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace App
+{
+    public class app
+    {
+        private static string _configFileName = "config.yml";
+        public static string _currentDirectory;
+        static app()
+        {
+            _currentDirectory = Directory.GetCurrentDirectory();
+        }
+        private static bool IsWorkingDirectory()
+        {
+            if (Directory.GetFiles(_currentDirectory).Contains(_configFileName) && Directory.GetDirectories(_currentDirectory).Contains("pages") && Directory.GetDirectories(_currentDirectory).Contains("app"))
+            {
+                return true;
+            }
+            return false;
+        }
+        public static DirectoryInfo SetupWorkingDirectory()
+        {
+            if (!IsWorkingDirectory())
+            {
+                File.Create(_configFileName);
+                Directory.CreateDirectory("pages");
+                Directory.CreateDirectory("app");
+                //здесь можно провести дополнительную конфигураци
+            }
+            return new DirectoryInfo(_currentDirectory);
+        }
+    }
+}
